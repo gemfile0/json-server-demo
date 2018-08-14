def09cb62751e8d548b1074cd42c86aba57a7469
박기용 <gemfile0@gmail.com>
2018-08-15 01:07:00 +0900

    # Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+    const faker = require("faker");
+    const _ = require("lodash");
+    
+    return {
+        people: _.times(100, function (n) {
+            return {
+                id: n,
+                name: faker.name.findName(),
+                avatar: faker.internet.avatar()
+            }
+        })
+    }
+}
